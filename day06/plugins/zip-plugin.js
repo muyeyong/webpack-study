d72644e1a7aa7f5b2abfe377bb76cb4b232d8ed7
muyeyong <xuyongshuaige@gmail.com>
2022-07-31 20:57:04 +0800

    feat: 尝试实现zip plugins

--- day06/plugins/zip-plugin.js
+++ day06/plugins/zip-plugin.js
@@ -1,8 +1,27 @@
+const zip = new require('jszip')()
+const RawSource = require('webpack-sources').RawSource;
 module.exports = class Zip {
   constructor(options) {
     this.options = options
   }
   apply(compiler) {
-    console.log('startup zipPlugin')
+    compiler.hooks.emit.tapAsync('ZipPlugin', (compilation, callback) => {
+      /* 
+        获取资源
+        打包
+        将打包结果挂载到asset上
+       */
+
+      // console.log(compilation.assets)
+      const zipFolder = zip.folder(this.options.fileName)
+      for (let assetName in compilation.assets) {
+        const source = compilation.assets[assetName].source()
+        zipFolder.file(assetName, source)
+      }
+      zip.generateAsync({ type: 'nodebuffer'}).then(content => {
+        compilation.assets[`${this.options.fileName}.zip`] = new RawSource(content)
+        callback()
+      })
+    })
   }
 }
